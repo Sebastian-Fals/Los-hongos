999ac62190ffed75c640f50160f73f847e3ef010
Sebastian Fals <sebastian.fals.lopez@gmail.com>
2021-11-03 20:20:29 -0500

    proceso 1 de mejorado de pagina

--- Js/index.js
+++
@@ -1,5 +0,0 @@
-function NuevaPagina()
-{
-    var PaginaDeseada = 
-    window.open('Top_5_setas_comestibles_mas_populares_del_mundo.html');
-}
--- index.js
+++
@@ -1,4 +0,0 @@
-function NuevaPagina()
-{
-    window.open('Top_5_setas_comestibles_mas_populares_del_mundo.html');
-}
